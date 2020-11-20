f08034c3f5e150398cc13d6fa910c50ac32e4840
alan <alanarostegui@hotmail.com>
2020-11-20 13:40:30 -0600

    enhancement(sockets): get band data through sockets

--- models/sockets.js
+++ models/sockets.js
@@ -1,31 +1,23 @@
-
+const BandList = require("./band-list.js");
 
 class Sockets {
+  constructor(io) {
+    this.io = io;
 
-    constructor( io ) {
+    this.bandList = new BandList();
+    this.socketEvents();
+  }
 
-        this.io = io;
-
-        this.socketEvents();
-    }
-
-    socketEvents() {
-        // On connection
-        this.io.on('connection', ( socket ) => {
-
-            // Escuchar evento: mensaje-to-server
-            socket.on('mensaje-to-server', ( data ) => {
-                console.log( data );
-                
-                this.io.emit('mensaje-from-server', data );
-            });
-            
+  socketEvents() {
+    // On connection
+    this.io.on("connection", (socket) => {
+      // Escuchar evento: mensaje-to-server
+      console.log("Cliente conectado");
         
-        });
-    }
-
-
+      // Emit all the current bands to the connected cliente
+      socket.emit('current-band', this.bandList.getBands())
+    });
+  }
 }
-
 
 module.exports = Sockets;
